d5e57f64dd720f5917d9e866f3abbed418e60dc1
kimhyeyun <cqqwer@gmail.com>
2020-04-30 22:07:38 +0900

    String & Number

---
+++ String/3.py
@@ -0,0 +1,6 @@
+print('hello world'.capitalize())
+print('hello world'.upper())
+print('hello world'.__len__())
+print(len('hello world'))
+print('Hello world'.replace('world', 'programming'))
+#replace: 일치하는 모든 문자열을 변경
