d4d77a30684da95d02f2b2919983c0f976c4dfe6
kimhyeyun <cqqwer@gmail.com>
2020-04-30 22:08:02 +0900

    Conditional Statement

---
+++ Conditional/3.py
@@ -0,0 +1,8 @@
+input = 11
+real = 11
+if real == input:
+    print("Hello!")
+else:
+    print("who are you?")
+
+#python에서는 들여쓰기가 중요!
---
+++ Conditional/3.rb
@@ -0,0 +1,8 @@
+input = 11
+real = 11
+if real == input
+  puts("Hello")
+else
+  puts("Who are you?")
+end
+#python과 달리 들여쓰기와 관련없이 end가 중요
