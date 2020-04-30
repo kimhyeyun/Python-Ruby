40185917b29d6d2ca0ed756b503b009a434dec0d
kimhyeyun <cqqwer@gmail.com>
2020-04-30 21:42:23 +0900

    FIRST DAY

---
+++ String/3.rb
@@ -0,0 +1,5 @@
+puts('hello world'.capitalize())
+puts('hello world'.upcase())
+puts('Hello world'.length())
+puts('Hello world'.sub('world', 'programming'))
+#sub: 맨처음 나타나는 문자열만  변경
---
+++ Variable/3.py
@@ -0,0 +1,5 @@
+# your code goes here
+donation = 200
+student = 10
+sponsor = 100
+print((donation*student)/sponsor)
