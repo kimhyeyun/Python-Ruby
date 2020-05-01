f575fc79f603083422c5fdc1705cf5557186dbf9
kimhyeyun <cqqwer@gmail.com>
2020-05-01 19:57:01 +0900

    Module

---
+++ Module/2.py
@@ -0,0 +1,9 @@
+#모듈이 없을 때
+
+def egoing_a():
+    return 'a'
+#엄청 많은 코드
+def k8005_a():
+    return 'B'
+#엄청 많은 코드
+print(egoing_a())
---
+++ Module/2.rb
@@ -0,0 +1,11 @@
+#모듈이 없을 때
+
+def egoing_a()
+  return 'a'
+end
+#엄청 많은 코드
+def k8805_a()
+  return 'B'
+end
+#엄청 많은 코드
+puts(egoing_a())
